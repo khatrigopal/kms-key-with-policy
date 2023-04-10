6df69ab326d6d41a41728802d97d41d52fe25a8b
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 08:06:09 +0100

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -9,7 +9,7 @@ resource "aws_kms_key" "test_key" {
   enable_key_rotation     = true
   key_usage  = "ENCRYPT_DECRYPT"
   
-  customer_master_key_spec = SYMMETRIC_DEFAULT
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
   is_enabled               = true
   
  }
