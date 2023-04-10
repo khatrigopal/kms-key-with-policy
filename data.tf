7fe173732077eb368879bd3efa61ce848b1062f5
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 14:11:57 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,5 +1,7 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_iam_user" "current" {}
+
 data "aws_iam_policy_document" "kms_key_policy" {
   # Allow root users full management access to key
   
