a58099e871b23904199fc16681f0e27316b2a143
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 10:33:37 +0100

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -13,6 +13,7 @@ resource "aws_kms_key" "test_key" {
   
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
   is_enabled               = true
+  policy = data.aws_iam_policy_document.test_kms_policy.json
   
  }
 
@@ -20,32 +21,33 @@ resource "aws_kms_key" "test_key" {
 data "aws_iam_policy_document" "test_kms_policy" {
   # Allow root users full management access to key
   statement {
-    sid = "AllowUseOfTheKey"
+   
     effect = "Allow"
     actions = ["kms:Encrypt", "kms:Decrypt"]
     
     resources = [aws_kms_key.test_key.arn]
-    #principals {
-     # type        = "AWS"
+    principals {
+     type        = "AWS"
       #identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-    #}
+      identifiers = ["*"]
+    }
   }
 }
 
-resource "aws_iam_policy" "kms_policy" {
-  name   = "testing-kms-policy"
-  policy = data.aws_iam_policy_document.test_kms_policy.json
-}
+#resource "aws_iam_policy" "kms_policy" {
+ # name   = "testing-kms-policy"
+  #policy = data.aws_iam_policy_document.test_kms_policy.json
+#}
 
-resource "aws_kms_key_policy" "test_kms_policy" {
-  key_id = aws_kms_key.test_key.key_id
-  policy = aws_iam_policy.kms_policy.policy
-}
+#resource "aws_kms_key_policy" "test_kms_policy" {
+ # key_id = aws_kms_key.test_key.key_id
+  #policy = aws_iam_policy.kms_policy.policy
+#}
 
-resource "aws_kms_alias" "test_key_alias" {
-  name          = var.key_alias
-  target_key_id = aws_kms_key.test_key.key_id
-}
+#resource "aws_kms_alias" "test_key_alias" {
+ # name          = var.key_alias
+  #target_key_id = aws_kms_key.test_key.key_id
+#}
 
 output "kms_key_arn" {
   value = aws_kms_key.test_key.arn
