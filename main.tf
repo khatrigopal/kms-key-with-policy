fedf04b42486f34c2f8d48f4c0583e006b98a6c2
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 07:54:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 module "test_kms_key" {
   source = "./modules/kms-key"
-
-  key_name        = "first-test-key"
-  key_description = "First Test KMS key"
+  key_alias  = "alias/my-kms-key
+  #key_name        = "first-test-key"
+  #key_description = "First Test KMS key"
 
   
+}
+variable "key_alias" {
+  description = "The alias to associate with the KMS key"
+  type        = string
+  default = ""
 }
 
 output "kms_key_arn" {
