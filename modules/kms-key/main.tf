8703becd5620f0c32297f7153f89a3e39d2f68d5
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 11:03:04 +0100

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -20,22 +20,6 @@ resource "aws_kms_key" "test_key" {
  }
 
 
-data "aws_iam_policy_document" "test_kms_policy" {
-  # Allow root users full management access to key
-  statement {
-    sid = "Allow Access"
-    effect = "Allow"
-    actions = ["kms:Encrypt", "kms:Decrypt"]
-    
-    resources = [aws_kms_key.test_key.arn]
-    principal = {
-      #AWS = "*"
-    #}
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-     # identifiers = ["*"]
-    }
-  }
-}
 
 
 resource "aws_iam_policy" "kms_policy" {
