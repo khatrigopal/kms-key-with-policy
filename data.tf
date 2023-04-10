640dad25e953134308bd25a8dbef24f734765eec
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 12:47:04 +0100

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "kms_key_policy" {
+  # Allow root users full management access to key
+  statement {
+    
+    effect = "Allow"
+    principals = {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrytpt
+    ]
+    resources=["*"]
+ }
+ }
+      
